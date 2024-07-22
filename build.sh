ecdf9534e90bd164c6377482ef8d4fe0dffaafc8
joocer <justin.joyce@joocer.com>
2024-07-22 18:56:32 +0100

    0.0.166

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+cd $GITHUB_WORKSPACE/io
+cd io
+
+# Only build for the specified Python version
+PYBIN="/opt/python/cp${PYTHON_VERSION//.}-cp${PYTHON_VERSION//.}/bin"
+
+# Install necessary packages
+"${PYBIN}/python" -m pip install -U setuptools wheel numpy==1.* cython
+
+# Build the wheel
+"${PYBIN}/python" setup.py bdist_wheel
+
+# Repair the wheel using auditwheel
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w dist/
+done
