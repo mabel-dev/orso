88c7946bfd39664ea54000d4b87abcfb3312e5aa
joocer <justin.joyce@joocer.com>
2025-03-06 22:29:39 +0000

    0.0.191

--- orso/cityhash/__init__.py
+++ orso/cityhash/__init__.py
@@ -1,2 +1,11 @@
-from cityhash import CityHash32
-from cityhash import CityHash64
+from opteryx.third_party.cyan4973.xxhash import hash_bytes
+
+def CityHash32(data: bytes) -> int:
+    if not isinstance(data, bytes):
+        data = str(data).encode()
+    return hash_bytes(data) & 0xFFFFFFFF
+
+def CityHash64(data: bytes) -> int:
+    if not isinstance(data, bytes):
+        data = str(data).encode()
+    return hash_bytes(data) & 0xFFFFFFFFFFFFFFFF
